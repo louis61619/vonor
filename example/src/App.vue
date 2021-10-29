035402f6be87f4b2c2be51dcb7bb51018b804095
louis <louis61619@gmail.com>
2021-10-29 18:04:28 +0800

    feat(docs): add new docs
    affects: @vonor/admin, @vonor/eslint-config

--- example/src/App.vue
+++ example/src/App.vue
@@ -8,7 +8,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
 // 局部引入
 // import { ElButton } from 'element-plus'
 // 局部引入css
--- packages/vonor-eslint-config/index.js
+++ packages/vonor-eslint-config/index.js
@@ -3,15 +3,23 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ],
   parserOptions: {
-    parser: 'babel-eslint'
+    ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 'warn',
-    'vue/no-unused-components': 'warn'
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-var-requires': 'off'
   },
   overrides: [
     {
